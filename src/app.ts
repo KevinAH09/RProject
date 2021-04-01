5a2066a062167f4c422ef4f15dfcf39520e1cff8
Gus2730 <gus2730@gmail.com>
2021-03-31 22:53:42 -0600

    cambios

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,14 @@ import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from "type-graphql"
 
 import { PingResolver } from "./resolvers/ping.resolver";
-import { ProductResolver } from "./resolvers/product.resolver";
+import { TipoServicioResolver } from "./resolvers/product.resolver";
 import { UserResolver } from './resolvers/user.resolver';
 import { isAuthorizated } from "./middleware/is-authorizated";
 export async function startServer() {
     const app = express();
     const server = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [PingResolver, ProductResolver, UserResolver],
+            resolvers: [PingResolver, TipoServicioResolver, UserResolver],
             authChecker: isAuthorizated
         }),
         context: ({ req, res }) => ({ req, res }),
