6fe9517355c838a77c71b7ce12b08ebce0c27aa6
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2021-04-03 09:34:15 -0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import { ApolloServer } from 'apollo-server-express'
 
 import { buildSchema } from "type-graphql"
 
-import { PingResolver } from "./resolvers/ping.resolver";
 import { TipoServicioResolver } from "./resolvers/tipo-servicio.resolver";
 import { TipoBeneficioResolver } from "./resolvers/tipo-beneficio.resolver";
 import { ConstruccionResolver } from "./resolvers/construccion.resolver";
@@ -11,11 +10,13 @@ import { UsuarioResolver } from './resolvers/usuario.resolver';
 import { CategoriaResolver } from './resolvers/categoria.resolver';
 import { SubcategoriaResolver } from './resolvers/subcategoria.resolver';
 import { isAuthorizated } from "./middleware/is-authorizated";
+import { FotoResolver } from './resolvers/foto.resolver';
+
 export async function startServer() {
     const app = express();
     const server = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [PingResolver, TipoServicioResolver, TipoBeneficioResolver,UsuarioResolver,ConstruccionResolver,CategoriaResolver,SubcategoriaResolver],
+            resolvers: [TipoServicioResolver, TipoBeneficioResolver,UsuarioResolver,ConstruccionResolver,CategoriaResolver,SubcategoriaResolver,FotoResolver],
             authChecker: isAuthorizated
         }),
         context: ({ req, res }) => ({ req, res }),
